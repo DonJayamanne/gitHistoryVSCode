87ff099f2101b209313b4a753949ec9770b7f6c6
Don Jayamanne <don.jayamanne@yahoo.com>
2018-07-13 10:27:02 -0700

    Author names can be empty (#295)

--- src/adapter/parsers/actionDetails/parser.ts
+++ src/adapter/parsers/actionDetails/parser.ts
@@ -7,7 +7,7 @@ export class ActionDetailsParser implements IActionDetailsParser {
     public parse(name: string, email: string, unixTime: string): ActionedDetails | undefined {
         name = (name || '').trim();
         unixTime = (unixTime || '').trim();
-        if (name.length === 0 || unixTime.length === 0) {
+        if (unixTime.length === 0) {
             return;
         }
 
