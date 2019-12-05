4be332810d51e45ef5cdd6e012839be36661f5f7
ole1986 <ole.k@web.de>
2019-12-05 15:09:59 +0100

    Added files search box into commit view #393

--- browser/src/components/LogView/Commit/index.tsx
+++ browser/src/components/LogView/Commit/index.tsx
@@ -18,20 +18,38 @@ interface CommitProps {
     selectCommittedFile: typeof ResultActions.selectCommittedFile;
 }
 
+interface CommitState {
+    searchText: string;
+}
+
+
 // const ContainerStyle = { width: '50%' };
 const ContainerStyle = {
     width: '100vw',
     height: '100vh'
 };
 
-class Commit extends React.Component<CommitProps> {
+class Commit extends React.Component<CommitProps, CommitState> {
+    private ref: HTMLInputElement;
+    constructor(props: CommitProps) {
+        super(props);
+        this.state = { searchText: ''};
+    }
+
     public componentDidUpdate() {
         // componentDidUpdate
+        this.ref.focus();
     }
 
     public componentDidMount() {
         // componentDidMount
+        this.setState({ searchText: '' });
     }
+
+    componentWillReceiveProps(nextProps: CommitProps): void {
+        this.setState({ searchText: '' });
+    }
+
     private onSelectFile = (fileEntry: CommittedFile) => {
         this.props.selectCommittedFile(this.props.selectedEntry, fileEntry);
     }
@@ -39,8 +57,21 @@ class Commit extends React.Component<CommitProps> {
         this.props.closeCommitView();
     }
     private renderFileEntries() {
-        return this.props.selectedEntry.committedFiles
+        if (this.state.searchText) {
+            return this.props.selectedEntry.committedFiles.filter(x => x.relativePath.indexOf(this.state.searchText) !== -1)
             .map((fileEntry, index) => <FileEntry theme={this.props.theme} committedFile={fileEntry} key={index + fileEntry.relativePath} onSelect={this.onSelectFile} />);
+        } else {
+            return this.props.selectedEntry.committedFiles
+            .map((fileEntry, index) => <FileEntry theme={this.props.theme} committedFile={fileEntry} key={index + fileEntry.relativePath} onSelect={this.onSelectFile} />);
+        }
+    }
+    private handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ searchText: e.target.value });
+    }
+    private handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Escape') {
+            this.setState({ searchText: '' });
+        }
     }
     public render() {
         const resizing = { top: true, right: false, bottom: false, left: false, topRight: false, bottomRight: false, bottomLeft: false, topLeft: false };
@@ -49,28 +80,29 @@ class Commit extends React.Component<CommitProps> {
             // tslint:disable-next-line:react-this-binding-issue
             <Rnd className='details-view-cnt' default={ContainerStyle} minWidth={50} minHeight={50} maxHeight='50%' bounds='parent'
                 enableResizing={resizing}  disableDragging={this.props.selectedEntry !== undefined}>
-                <div className='detailsCnt'>
-                    <div id='details-view' >
-                        <a role='button' className='action-btn close-btn' onClick={this.onClose}><GoX></GoX></a>
-                        <div className='authorAndCommitInfoContainer'>
-                            <Avatar result={this.props.selectedEntry.author}></Avatar>
-                            <h1 className='commit-subject'>
-                                {gitmojify(this.props.selectedEntry.subject)}
-                                &nbsp;
-                                <CopyToClipboard text={this.props.selectedEntry.subject + "\n" + this.props.selectedEntry.body}>
-                                    <span className='btnx clipboard hint--right hint--rounded hint--bounce' aria-label='Copy commit text'>
-                                        <GoClippy></GoClippy>
-                                    </span>
-                                </CopyToClipboard>
-                            </h1>
-                            <Author result={this.props.selectedEntry.author}></Author>
-                            <div className='commit-body'>{gitmojify(this.props.selectedEntry.body)}</div>
-                            <div className='commit-notes'>{gitmojify(this.props.selectedEntry.notes)}</div>
-                        </div>
-                        {this.renderFileEntries()}
-                        <ul className='committed-files'>
-                        </ul>
+                <div id='details-view'>
+                    <a role='button' className='action-btn close-btn' onClick={this.onClose}><GoX></GoX></a>
+                    <div className='authorAndCommitInfoContainer'>
+                        <Avatar result={this.props.selectedEntry.author}></Avatar>
+                        <h1 className='commit-subject'>
+                            {gitmojify(this.props.selectedEntry.subject)}
+                            &nbsp;
+                            <CopyToClipboard text={this.props.selectedEntry.subject + "\n" + this.props.selectedEntry.body}>
+                                <span className='btnx clipboard hint--right hint--rounded hint--bounce' aria-label='Copy commit text'>
+                                    <GoClippy></GoClippy>
+                                </span>
+                            </CopyToClipboard>
+                        </h1>
+                        <Author result={this.props.selectedEntry.author}></Author>
+                        <div className='commit-body'>{gitmojify(this.props.selectedEntry.body)}</div>
+                        <div className='commit-notes'>{gitmojify(this.props.selectedEntry.notes)}</div>
                     </div>
+                </div>
+                <div id="comitted-files">
+                    <div>
+                        <input ref={x => {this.ref = x;} } className={'textInput'} type="text" value={this.state.searchText} placeholder="Find file" onKeyDown={this.handleKeyDown} onChange={this.handleSearchChange} />
+                    </div>
+                    {this.renderFileEntries()}
                 </div>
             </Rnd >);
     }
