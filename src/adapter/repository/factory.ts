3a38f020924d8ff6037fc8af5db960f3a684011f
ole1986 <ole.k@web.de>
2019-12-22 18:31:22 +0100

    Slightly amended repo index

--- src/adapter/repository/factory.ts
+++ src/adapter/repository/factory.ts
@@ -12,38 +12,39 @@ import { IGitArgsService } from './types';
 export class GitServiceFactory implements IGitServiceFactory {
     private readonly gitServices = new Map<number, IGitService>();
     private gitApi: API;
+    private repoIndex: number;
     constructor(@inject(IGitCommandExecutor) private gitCmdExecutor: IGitCommandExecutor,
         @inject(ILogParser) private logParser: ILogParser,
         @inject(IGitArgsService) private gitArgsService: IGitArgsService,
         @inject(IServiceContainer) private serviceContainer: IServiceContainer) {
 
         this.gitApi = this.gitCmdExecutor.gitExtension.getAPI(1);
+        this.repoIndex = -1;
     }
 
     public async createGitService(resource?: Uri | string): Promise<IGitService> {
         const resourceUri = typeof resource === 'string' ? Uri.file(resource) : resource;
-        let repoIndex: number = -1;
 
         if (!resourceUri) {
             // no git service initialized, so take the selected as primary
-            repoIndex = this.gitApi.repositories.findIndex(x => x.ui.selected);
+            this.repoIndex = this.gitApi.repositories.findIndex(x => x.ui.selected);
         }
 
-        if (repoIndex === -1) {
+        if (this.repoIndex === -1) {
             // find the correct repository from the given resource uri
             this.gitApi.repositories.forEach((x, i) => {
                 if (resourceUri!.fsPath.startsWith(x.rootUri.fsPath)) {
-                    if (repoIndex === -1 || x.rootUri.fsPath.length > this.gitApi.repositories[repoIndex].rootUri.fsPath.length) {
-                        repoIndex = i;
+                    if (this.repoIndex === -1 || x.rootUri.fsPath.length > this.gitApi.repositories[this.repoIndex].rootUri.fsPath.length) {
+                        this.repoIndex = i;
                     }
                 }
             });
         }
 
-        if (!this.gitServices.has(repoIndex)) {
-            this.gitServices.set(repoIndex, new Git(this.gitApi.repositories[repoIndex], this.serviceContainer, this.gitCmdExecutor, this.logParser, this.gitArgsService));
+        if (!this.gitServices.has(this.repoIndex)) {
+            this.gitServices.set(this.repoIndex, new Git(this.gitApi.repositories[this.repoIndex], this.serviceContainer, this.gitCmdExecutor, this.logParser, this.gitArgsService));
         }
 
-        return this.gitServices.get(repoIndex)!;
+        return this.gitServices.get(this.repoIndex)!;
     }
 }
