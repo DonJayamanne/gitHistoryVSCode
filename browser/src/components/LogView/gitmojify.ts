449891710a020d34523d954021c555612be30ce2
ole1986 <ole.k@web.de>
2020-02-11 15:21:55 +0100

    Replaced gemoji submodule with node-emoji package

--- browser/src/components/LogView/gitmojify.ts
+++ browser/src/components/LogView/gitmojify.ts
@@ -1,22 +1,5 @@
-// tslint:disable-next-line:no-require-imports no-var-requires
-const emojis = require('../../../../gemoji/db/emoji.json');
-
-type Gitmoji = {
-    // The long emoji code with colons.
-    code: string;
-
-    // The actual emoji.
-    emoji: string;
-};
-
-const gitmojis: Gitmoji[] = [];
-// tslint:disable-next-line:no-any
-emojis.forEach((e: any) => e.aliases.forEach((alias: string) => gitmojis.push({
-    code: `:${alias}:`,
-    emoji: e.emoji
-})));
+import * as emoji from 'node-emoji';
 
 export function gitmojify(message: string): string {
-    gitmojis.forEach((gitmoji: Gitmoji) => message = message.replace(gitmoji.code, gitmoji.emoji));
-    return message;
+    return emoji.emojify(message)
 }
