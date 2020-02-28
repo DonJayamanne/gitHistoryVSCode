557f4a3368fb676ae10ad8456f683d2aea5af5bb
ole <ole.k@web.de>
2020-02-28 17:22:39 +0100

    Update src/extension.ts
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,8 @@
-import 'reflect-metadata';
+// This line should always be right on top.
+// Other extensions use this, re-importing will cause data stored by this to wipe out data in other extensions using this same package.
+if ((Reflect as any).metadata === undefined) {
+    require('reflect-metadata');
+}
 
 import { Container } from 'inversify';
 import * as vscode from 'vscode';
