318ecdf25eec724543ce38478571a3ad8e6bcca4
Don Jayamanne <don.jayamanne@yahoo.com>
2020-03-09 18:52:34 -0700

    Comments

--- test/extension/extension.test.ts
+++ test/extension/extension.test.ts
@@ -3,6 +3,7 @@ import * as vscode from 'vscode';
 
 describe('Extension Test Suite', () => {
     test('Sample test', async () => {
+        // We can use the real thing.
         assert.ok(typeof vscode.env.appName === 'string');
         // await vscode.window.showErrorMessage('Hello');
         assert.equal([1, 2, 3].indexOf(5), -1);
