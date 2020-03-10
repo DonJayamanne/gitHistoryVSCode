e0977b4f338a97c39aaaaa182f3a97308b28955d
Don Jayamanne <don.jayamanne@yahoo.com>
2020-03-09 18:52:14 -0700

    Comment code

--- test/extension/extension.test.ts
+++ test/extension/extension.test.ts
@@ -4,7 +4,7 @@ import * as vscode from 'vscode';
 describe('Extension Test Suite', () => {
     test('Sample test', async () => {
         assert.ok(typeof vscode.env.appName === 'string');
-        await vscode.window.showErrorMessage('Hello');
+        // await vscode.window.showErrorMessage('Hello');
         assert.equal([1, 2, 3].indexOf(5), -1);
         assert.equal([1, 2, 3].indexOf(0), -1);
     });
