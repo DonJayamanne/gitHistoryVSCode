4eb70e084524d6699262f2a477f3b9628c5baa7d
ole1986 <ole.k@web.de>
2020-11-01 16:46:54 +0100

    Use native "getBranches" from gite RepositoryState

--- src/adapter/repository/git.d.ts
+++ src/adapter/repository/git.d.ts
@@ -3,7 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { CancellationToken, Event, SourceControlInputBox, Uri } from 'vscode';
+import { Uri, Event, Disposable, ProviderResult } from 'vscode';
+export { ProviderResult } from 'vscode';
 
 export interface Git {
     readonly path: string;
@@ -41,7 +42,10 @@ export interface Commit {
     readonly hash: string;
     readonly message: string;
     readonly parents: string[];
-    readonly authorEmail?: string | undefined;
+    readonly authorDate?: Date;
+    readonly authorName?: string;
+    readonly authorEmail?: string;
+    readonly commitDate?: Date;
 }
 
 export interface Submodule {
@@ -110,9 +114,29 @@ export interface RepositoryUIState {
     readonly onDidChange: Event<void>;
 }
 
+/**
+ * Log options.
+ */
 export interface LogOptions {
-    // Max number of log entries to retrieve. If not specified, the default is 32.
+    /** Max number of log entries to retrieve. If not specified, the default is 32. */
     readonly maxEntries?: number;
+    readonly path?: string;
+}
+
+export interface CommitOptions {
+    all?: boolean | 'tracked';
+    amend?: boolean;
+    signoff?: boolean;
+    signCommit?: boolean;
+    empty?: boolean;
+    noVerify?: boolean;
+}
+
+export interface BranchQuery {
+    readonly remote?: boolean;
+    readonly pattern?: string;
+    readonly count?: number;
+    readonly contains?: string;
 }
 
 export interface Repository {
@@ -153,6 +177,7 @@ export interface Repository {
     createBranch(name: string, checkout: boolean, ref?: string): Promise<void>;
     deleteBranch(name: string, force?: boolean): Promise<void>;
     getBranch(name: string): Promise<Branch>;
+    getBranches(query: BranchQuery): Promise<Ref[]>;
     setBranchUpstream(name: string, upstream: string): Promise<void>;
 
     getMergeBase(ref1: string, ref2: string): Promise<string>;
@@ -162,6 +187,7 @@ export interface Repository {
 
     addRemote(name: string, url: string): Promise<void>;
     removeRemote(name: string): Promise<void>;
+    renameRemote(name: string, newName: string): Promise<void>;
 
     fetch(remote?: string, ref?: string, depth?: number): Promise<void>;
     pull(unshallow?: boolean): Promise<void>;
@@ -169,6 +195,40 @@ export interface Repository {
 
     blame(path: string): Promise<string>;
     log(options?: LogOptions): Promise<Commit[]>;
+
+    commit(message: string, opts?: CommitOptions): Promise<void>;
+}
+
+export interface RemoteSource {
+    readonly name: string;
+    readonly description?: string;
+    readonly url: string | string[];
+}
+
+export interface RemoteSourceProvider {
+    readonly name: string;
+    readonly icon?: string; // codicon name
+    readonly supportsQuery?: boolean;
+    getRemoteSources(query?: string): ProviderResult<RemoteSource[]>;
+    publishRepository?(repository: Repository): Promise<void>;
+}
+
+export interface Credentials {
+    readonly username: string;
+    readonly password: string;
+}
+
+export interface CredentialsProvider {
+    getCredentials(host: Uri): ProviderResult<Credentials>;
+}
+
+export interface PushErrorHandler {
+    handlePushError(
+        repository: Repository,
+        remote: Remote,
+        refspec: string,
+        error: Error & { gitErrorCode: GitErrorCodes },
+    ): Promise<boolean>;
 }
 
 export type APIState = 'uninitialized' | 'initialized';
@@ -180,6 +240,14 @@ export interface API {
     readonly repositories: Repository[];
     readonly onDidOpenRepository: Event<Repository>;
     readonly onDidCloseRepository: Event<Repository>;
+
+    toGitUri(uri: Uri, ref: string): Uri;
+    getRepository(uri: Uri): Repository | null;
+    init(root: Uri): Promise<Repository | null>;
+
+    registerRemoteSourceProvider(provider: RemoteSourceProvider): Disposable;
+    registerCredentialsProvider(provider: CredentialsProvider): Disposable;
+    registerPushErrorHandler(handler: PushErrorHandler): Disposable;
 }
 
 export interface GitExtension {
@@ -216,6 +284,7 @@ export const enum GitErrorCodes {
     CantOpenResource = 'CantOpenResource',
     GitNotFound = 'GitNotFound',
     CantCreatePipe = 'CantCreatePipe',
+    PermissionDenied = 'PermissionDenied',
     CantAccessRemote = 'CantAccessRemote',
     RepositoryNotFound = 'RepositoryNotFound',
     RepositoryIsLocked = 'RepositoryIsLocked',
